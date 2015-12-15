d8143af0c9cc516dacca7df174287081f19074f4
Brian Bancroft <bancroft.bw@gmail.com>
2015-12-15 11:56:21 -0700

    More meddling...
    Alright. This script works now. It is now capable of taking in a single
    plt track from Ozi Explorer and pooping out a single point feature
    classshape for UTM zone 12N in NAD 83. Further updates will be meant to
    clean this, so that it can be treated as a single class!

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import os, arcpy
+import os, arcpy, time
 inFile = "C:\\scripting\\sample.txt"
 '''Some notes about the file:
 If you break the thing by line starting at 0, here are the lines:
@@ -52,14 +52,18 @@ For the actual scripting, I may make a class or function that does the actual wo
 endProgram = False
 endMessage = ["","I am unable to deal with that coordinate system", "this file doesn't look right"]
 # Input track file (single at first)
-inTrack = ""
-outFile = ""
-outPath = ""
 
+outPath = "C:\Dropbox\\2. Active Jobs\\Brion 15-16\\shp\\CustomAnalytics.gdb\\testing\\"
+inTrack = "C:\\scripting\\sample.txt"
+outFile = "test1"
+sr_UTM = arcpy.SpatialReference(26912)
+
+'''Time zone info: This might not be required, but if the output time is in Zulu, I'm going to have to carry out
+some major conversion here
 # Input Time Zone
 timeZone = ""
 # Input Daylight Savings
-isDaylightSavings = True
+isDaylightSavings = True'''
 
 
 # Get data
@@ -90,62 +94,63 @@ if record[0] != 'OziExplorer Track Point File Version 2.1\n':
     errorMessage = 2
 if record[1] != 'WGS 84\n':
     endProgram = True
-    sr = arcpy.SpatialReference(4326)
     errorMessage = 1
+else:
+    sr = arcpy.SpatialReference(4326)
 if record[2] != 'Altitude is in Feet\n':
     endProgram = True
-
-
-
-
-
 # If not file verification: Kill the program
 if not endProgram:
 
-    # Create Feature Class
-    arcpy.CreateFeatureclass_management(dir, outFile,"POINT", spatial_reference=sr)
 
-    # Add columns to feature class. Lat, Long, Altitude, GPS time, Date, Local Time. everything but time and date are float.
+    # Create Feature Class
+    arcpy.CreateFeatureclass_management(outPath, outFile,"POINT", spatial_reference=sr)
+    fc = outPath + outFile
+    # Add columns to feature class. Everything but time and date are float.
+
+    arcpy.AddField_management(fc, "Lat", "FLOAT")
+    arcpy.AddField_management(fc, "Long", "FLOAT")
+    arcpy.AddField_management(fc, "Altitude", "FLOAT")
+    arcpy.AddField_management(fc, "GPSTime", "FLOAT")
+    arcpy.AddField_management(fc, "Date","DATE")
+    arcpy.AddField_management(fc,"Time","DATE")
+
+
 
     count = 6
-    '''This is what a single line looks like when it's split:
-    ['  55.229164', '-118.522850', '1', ' 2284.0', '42135.6794792', ' 11-May-15', ' 4:18:27 PM\n']
-    '''
+
     while count < len(record):
         row = record[count].split(',')
-        lat =  float(row[0][2:])
-        long = float(row[1])
-        xy = (long,lat)
-        altitude = float(row[3])
-        gpsTime = float(row[4])
-        date = row[5]
-        time = row[6][:-1]
-
-        # verify the integrity of each of the coordinates. Lat needs to have only two characters before the decimal.
-
-
-
-        cursor = arcpy.da.InsertCursor(outPath+outFile,
-                                       ("SHAPE@XY", "Lat", "Long", "Altitude", "GPSTime", "Date", "Time"))
-
-        cursor.insertRow((xy,lat,long,altitude,gpsTime,date,time))
-
-        # The proper usage of insertCurrsor is: cursor.insertRow((xy,"words",date,time))
-        # in this while loop, create a new feature with geometry that matches the geometry on the bit. Add the other details in.
-        # Convert the time as well.
-
-
-        # END WHILE LOOP
-
-
-        # check topographic integrity of feature class
+        if len(row) == 7:
+            lat =  float(row[0][2:])
+            long = float(row[1])
+            xy = (long,lat)
+            altitude = float(row[3])
+            gpsTime = float(row[4])
+            date = row[5][1:]
+            ptTime = row[6][1:-1]
 
 
 
-        # convert feature class to UTM for accuracy. This class is the final name of the shapefile and should bear similarity to the track file.
+
+            # verify the integrity of each of the coordinates. Lat needs to have only two characters before the decimal.
+            cursor = arcpy.da.InsertCursor(fc,
+                                           ("SHAPE@XY", "Lat", "Long", "Altitude", "GPSTime", "Date", "Time"))
+
+            cursor.insertRow((xy,lat,long,altitude,gpsTime,date,ptTime))
+
+            del cursor
+        count = count + 1
+
+            # END WHILE LOOP
+            #end if statement
+
+    arcpy.RepairGeometry_management(fc)
+    arcpy.Project_management(fc, outPath + "track1", sr_UTM)
+    arcpy.Delete_management(fc)
 
 
-        # we're done here.
 
-        #
+
+
 
